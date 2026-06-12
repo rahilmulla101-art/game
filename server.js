138dddeb5fbeed862fef6ebc8462c75f387be038
rahilmulla101-art <rahilmulla101.com>
2026-06-12 10:56:23 +0530

    Hardcoded port and secret keys to fix Render timeout

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 10000;
 const app = express();
 const server = createServer(app);
 
