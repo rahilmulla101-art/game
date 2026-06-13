570c2956862ea4cefbf55de29e0a4f72ce263068
rahilmulla101-art <rahilmulla101.com>
2026-06-13 19:18:26 +0530

    Hardcoded port and akn sdasd l'amdasl;dsa ajhasjdh lksdjfslkdfaj gfdfgdgdfgcv jklsgjlkdsjfflk jjhadfjkhadsfjk hggffhgfhg fksdmlksdf  fhgfghfhgfghfyuk kdajfbadsjkfbajk jffhghg

--- routes/session.js
+++ routes/session.js
@@ -3,9 +3,13 @@ import { checkToken } from '../controllers/sessionController.js';
 
 const router = express.Router();
 
-router.post(
-    '/check-token',
-    checkToken
-);
+router.post('/check-token', (req,res)=>{
+    console.log("BODY TEST:", req.body);
+
+    res.json({
+        success:true,
+        body:req.body
+    });
+});
 
 export default router;
